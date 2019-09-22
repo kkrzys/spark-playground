4ad133fc6e6354f04e78eb459d25afa07d89a388
kkrzys <klimekkrzys@gmail.com>
2019-09-22 18:32:25 +0200

    refactoring

--- src/main/scala/playground/delta_io_example/DeltaExampleApp.scala
+++ src/main/scala/playground/delta_io_example/DeltaExampleApp.scala
@@ -13,14 +13,13 @@ object DeltaExampleApp extends App {
     .csv("src/main/resources/data/landing/topics/dev-eplstanding-receive/*.csv")
   data.write.format("delta").mode("overwrite").save(deltaTablePath)
 
-  //val updates = spark.range(10, 30).as("updates").toDF
-
   val deltaTable = DeltaTable.forPath(spark, deltaTablePath)
 
   deltaTable.updateExpr(
     "Team = 'Arsenal'",
     Map("2000" -> "'4'", "2001" -> "'4'"))
 
+  deltaTable.toDF.printSchema()
   deltaTable.toDF.show()
 }
 
